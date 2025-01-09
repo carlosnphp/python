b60a789a6f2563915cf0695994c9e54e593ecb19
Carlos Nascimento <carlosnascimentophp@gmail.com>
2025-01-09 10:45:37 -0300

    python entrada e saida e condional

---
+++ main.py
@@ -0,0 +1,41 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+
+'''
+#somando valores
+numberOne = (int(input("Digite o primeiro número: ")))
+numberTwo = (int(input("Digite o segundo número: ")))
+
+numberThir = numberOne + numberTwo
+
+print("Resultado é: ", numberThir)
+'''
+
+'''#subtraçao de valores
+
+numberOne = (float(input("Digite o numero maior: ")))
+numberTwo = (float(input("Digite o numero menor: ")))
+numberThir = numberOne - numberTwo
+
+print("O resultado é: ", numberThir)'''
+
+#CONDICIONAIS
+numberOne = (int(input("Digite o número: ")))
+numberTwo = (int(input("Digite o segundo número: ")))
+
+if numberOne > numberTwo:
+    print("O número ", numberOne, "é maior do que", numberTwo)
+
+elif numberOne < numberTwo:
+        print("O número ", numberOne, "é menor do que", numberTwo)
+
+else:
+            print("Os números ", numberOne, "e", numberTwo, "são iguais")
+
+
