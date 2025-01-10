7f106ee75bc58d3113bce7581b7164c8e26107fa
Carlos Nascimento <carlosnascimentophp@gmail.com>
2025-01-10 13:54:14 +0000

    incrementando condições si

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 '''
 
 Welcome to GDB Online.
@@ -25,6 +26,7 @@ numberThir = numberOne - numberTwo
 
 print("O resultado é: ", numberThir)'''
 
+'''
 #CONDICIONAIS
 numberOne = (int(input("Digite o número: ")))
 numberTwo = (int(input("Digite o segundo número: ")))
@@ -39,3 +41,71 @@ else:
             print("Os números ", numberOne, "e", numberTwo, "são iguais")
 
 
+'''
+#PAR OU IMPAR
+# number = (int(input("Digite um número para sabermos se é par ou ímpar: ")))
+
+# resto = number % 2
+
+# if resto == 0:
+#     print("Numero: ", number, "é par")
+# else:
+#     print("Número: ", number, "é ímpar")
+
+
+
+#variáveis
+
+'''
+é um elemento que armazena determinado valor na memória e que ao longo do códito varia (pode mudar)
+
+'''
+
+
+#Tipos de dados
+#int : inteiro
+#float : fracionado ou com vírgula
+#booleano: valor lógico true or false
+#str : estrings
+
+
+#Entradas e saídas
+
+'''
+#Entada é o envio da informação para o processamento do computador
+
+#Processamento é o tratamento para gerar um resultado
+
+#Saída é o resultado da informação após o processamento
+'''
+
+#SISTEMA SIMPLES
+ 
+#     Pedi um número, dizer se é ímpar ou par
+
+# python main.py (Comando para rodar o programa [main.py é o nome do arquivo])
+
+#  "==" : é o simbolo de igualdade
+
+# "=" : é o simbolo de atribuir um valor para a variável
+
+
+number = (int(input("\nDigite um número inteiro para saber se é PAR OU ÍMPAR: ")))
+
+resto = number % 2 ## pega o resto da divisão por 2##
+
+if resto == 0:
+    print("O número é par")
+else:
+    print("O número é impar")
+
+    if resto == 1 and resto <= 3 or resto <= 5:
+        print("Número está entre 3 e 5")
+    else:
+        print("Número é ÍMPAR, mas é maior do que 5")
+    
+        
+
+
+
+  
